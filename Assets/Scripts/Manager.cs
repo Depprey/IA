d1738e721b5fac95bb8b8c4729c8560635438632
lucasino <lucascalbo@gmail.com>
2017-10-10 10:40:35 +0200

    Scripts

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    
+    public Transform[] path;
+
+    public float speed = 5.0f;
+    public float reachDist = 0.2f;
+    public int i = 0;
+
+    private bool fin = false;
+
+    public Vector3 startPosition;
+    
+	void Start () {
+
+        startPosition = this.transform.position;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (fin == false)
+        {        
+        float dist = Vector3.Distance(path[i].position, transform.position);
+        transform.position = Vector3.Lerp(transform.position, path[i].position, Time.deltaTime * speed);
+
+        if (dist <= reachDist)
+        {
+            i++;
+        }
+            if (i >= path.Length)
+            {
+                fin = true;
+            }
+        }else if(fin == true)
+        {
+            float dist = Vector3.Distance(startPosition, transform.position);
+            if (dist >= reachDist)
+            {
+                transform.position = Vector3.Lerp(transform.position, startPosition, Time.deltaTime * speed);
+                dist = Vector3.Distance(startPosition, transform.position);
+            }
+        }
+    }    
+}
---
+++ Assets/Scripts/david.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class david : MonoBehaviour {
+
+    public float speed;
+    public GameObject[] target;
+    Vector3 inicio;
+    bool fin = false;
+    int i = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+        inicio = this.transform.position;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(fin == false)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, target[i].transform.position, speed * Time.deltaTime);
+            if (transform.position == target[i].transform.position)
+            {
+                i++;
+            }
+            if ( i == target.Length)
+            {
+                fin = true;
+            }
+
+        }else if (fin == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, inicio, speed * Time.deltaTime);
+            if (transform.position == inicio)
+                fin = false;
+        }
+
+	}
+}
