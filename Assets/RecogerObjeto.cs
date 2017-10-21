0400a60342e7e36ae8c51070c5d5c42e9bc9356c
Depprey <monarcrey@gmail.com>
2017-10-21 14:30:46 +0200

    Script RecogerObjeto + MeshRendererOff de todos los paths y los aaaa(?) para que quede mas bonito

---
+++ Assets/RecogerObjeto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecogerObjeto : MonoBehaviour {
+    //Me lo he inventado un poco el start pero igual es algo así, llamar al componente OffMeshLink con el tag "SaltoBueno" y darle un nombre, para luego activarlo?
+  /*void Start()
+    {
+        SaltoActivable = GameObject.FindGameObjectWithTag("SaltoBueno").GetComponent<OFfMeshLink>;
+    }
+    */
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Path16Objeto")
+        {
+            Destroy(col.gameObject);
+            GameObject SaltoActivable = GameObject.FindGameObjectWithTag("SaltoBueno");
+            SaltoActivable.SetActive(true);
+        }
+    }
+}
